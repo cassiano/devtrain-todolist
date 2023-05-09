a16c4074bed1c0c00f62ae04ac8edab02406448e
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2023-05-09 01:52:43 -0300

    Parte 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { FC } from 'react'
 import './App.css'
 import TaskManager from './components/task_manager'
 
-const App = () => <TaskManager />
+export type TaskType = {
+  id: number
+  title: string
+  done: boolean
+}
+
+// Mocked data.
+const TASKS: TaskType[] = [
+  { id: 1, title: 'A', done: true },
+  { id: 2, title: 'B', done: false },
+  { id: 3, title: 'C', done: true },
+]
+
+const App: FC = () => <TaskManager tasks={TASKS} />
 
 export default App
